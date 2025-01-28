c8a36ccfbdafe085d69791bb0ddeb905975fcf2b
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2025-01-28 10:40:10 -0700

    Added pseudocode text file

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # Music Festival Management System
 
+# Tuple of times of in standard notation
 currentTimes = ()
 currentTimeframes = []
 
-days = int(input("How many days are you going to have the festival be? :"))
+schedule = ()
 
-def updateCurrentTimes():
-    
+artist_list = [{'name': 'Eminem', 'genre': 'Rap', 'start': schedule[9], 'end': 11.30}]
+
+days = int(input("How many days are you going to have the festival be?:"))
+
+def ListArtists():
+    for artist in artist_list:
+        print(f'Name: {artist['name']}')
+        print(f'Genre: {artist['genre']}')
+
+ListArtists()
