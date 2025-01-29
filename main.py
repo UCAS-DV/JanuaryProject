09cf3e07471cb2e470eba26ce5b2ddeddb5a60ca
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2025-01-29 10:22:30 -0700

    do the stuff i guess

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 # Music Festival Management System
 
-# Tuple of times of in standard notation
 currentTimes = ()
 currentTimeframes = []
+schedule = []
 
-schedule = ()
+days = int(input("How many days are you going to have the festival be? :"))
+dayCount = 0
 
-artist_list = [{'name': 'Eminem', 'genre': 'Rap', 'start': schedule[9], 'end': 11.30}]
+def updateCurrentTimes(timeframes, startTime, endTime):
+    timeframeCount = timeframes
+    timeNow = startTime
+    timeNowHour = startTime
 
-days = int(input("How many days are you going to have the festival be?:"))
+    currentTimes = currentTimes + timeNow
+    currentTimes = currentTimes + timeNowHour
 
-def ListArtists():
-    for artist in artist_list:
-        print(f'Name: {artist['name']}')
-        print(f'Genre: {artist['genre']}')
+    while timeNow <= endTime:
+        remainder = timeframeCount % 2
+        if remainder == 1:
+            timeNowHour = timeNowHour + 1
+        elif remainder == 0:
+            timeNow = timeNow + .30
 
-ListArtists()
+if days >= dayCount:
+    start = float(input("What time does the performance start? (Minutes are after a decimal point, ex. 10.30)"))
+    end = float(input("What time does the performance end? (Minutes are after a decimal point, ex 12.30)"))
+    timeframes = end - start
+    timeframes = timeframes / 2
+    currentTimeframes = currentTimeframes + timeframes
+    updateCurrentTimes()
+
+def performancesInDay():
+    performInDay = int(input("How many performances are in this day?"))
+    if performInDay > 0:
+        
